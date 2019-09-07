a03caf5222e70eb1cbae7e22d9789849d4f3fa88
John Salas <johndsalas@gmail.com>
2019-09-06 19:45:32 -0500

    3.8.3

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -76,14 +76,65 @@ where dept_no in (	select dept_no
 					where emp_no in (select emp_no
 									 from employees
 									 where gender = "F")
-					
-					
-					
-						);
+				);
 
 -- Find the first and last name of the employee with the highest salary.
 
+select first_name, last_name
+
+from employees
+
+where emp_no in(
+				select emp_no
+				from salaries
+				where salary in( 
+								select max(salary) 
+								from salaries
+								where to_date > now()
+								)
+				);
+
+
 -- Find the department name that the employee with the highest salary works in.
+
+select dept_name
+
+from departments
+
+where dept_no in(
+				select dept_no
+				from dept_emp
+				where emp_no in(
+								select emp_no
+								from employees
+								where emp_no in(
+												select emp_no
+												from salaries
+												where salary in( 
+																select max(salary) 
+																from salaries
+																where to_date > now()
+															   )
+												)
+								)
+				);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
